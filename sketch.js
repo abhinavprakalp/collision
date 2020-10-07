2bbf275bbf481d677408ad15872adf2ea81aae66
abhinavprakalp <71956088+abhinavprakalp@users.noreply.github.com>
2020-10-07 17:54:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var mr,fr;
+
+function setup() {
+  createCanvas(800,400);
+  mr = createSprite(400, 200, 50, 50);
+  fr = createSprite(100, 200, 150, 50);
+
+  mr.shapeColor="red";
+  fr.shapeColor="red";
+}
+
+function draw() {
+  background("blue");
+
+  mr.x=World.mouseX;
+  mr.y=World.mouseY;
+
+  if(mr.x-fr.x<mr.width/2+fr.width/2 && 
+     fr.x-mr.x<mr.width/2+fr.width/2 && 
+     mr.y-fr.y<mr.height/2+fr.height/2 &&
+     fr.y-mr.y<mr.height/2+fr.height/2){
+    mr.shapeColor="green";
+    fr.shapeColor="green";
+  }
+  else {
+    mr.shapeColor="red";
+    fr.shapeColor="red";
+  }
+
+  drawSprites();
+}
